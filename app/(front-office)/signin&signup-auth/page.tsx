8e2807f4ac3c67e83f784edf0e9b0d9f5dbf678b
Seyi Adeniyi <adeniyiemmanuel042@gmail.com>
2026-05-11 13:46:25 +0100

    sigin-in fixed

--- app/(front-office)/signin&signup-auth/page.tsx
+++ app/(front-office)/signin&signup-auth/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import AuthPage from '@/components/_user/auth-page';
+import { PageLoader } from '@/components/loading-spinners';
 import { useAuthStore } from '@/stores/auth-stores';
 import { useRouter } from 'next/navigation';
+import { Suspense } from 'react';
 
 export default function AuthenticationPage() {
   const router = useRouter()
@@ -11,8 +13,9 @@ export default function AuthenticationPage() {
   }
 
   return (
-      
+      <Suspense fallback={<PageLoader/>}>
       <AuthPage />
+    </Suspense>
     
   )
 }
